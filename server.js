9c9b2eff2f74efbc790836b368291b4571511cce
Todd Stuke <tddstuke@gmail.com>
2022-05-29 17:12:16 -0500

    added relational table data

--- server.js
+++ server.js
@@ -15,9 +15,14 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the election database")
 );
+
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+  AS party_name
+  FROM candidates
+  LEFT JOIN parties
+  ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -33,7 +38,12 @@ app.get("/api/candidates", (req, res) => {
 
 // GET a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name
+  AS party_name
+  FROM candidates
+  LEFT JOIN parties
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
