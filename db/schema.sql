e2b15bee1d46e520b1b10c2b0083146a66575713
Todd Stuke <tddstuke@gmail.com>
2022-05-29 16:33:49 -0500

    added parties table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@
         last_name VARCHAR(30) NOT NULL,
         industry_connected BOOLEAN NOT NULL
     );
+
+    CREATE TABLE parties (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        description TEXT
+    );
